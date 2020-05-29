2f688362601a75317ad40c8767ce9f917ea2a390
Alexander Yshakov <Ushak0vAlexander@yandex.ru>
2020-05-30 01:10:00 +0400

    basic routing !without params for actions!

---
+++ controllers/catalogController.php
@@ -0,0 +1,9 @@
+<?php 
+    class catalogController {
+        public function index() {
+            print "catalog page. Finaly.";
+        }
+        public function error() {
+            print "Error in catalog";
+        }
+    }
