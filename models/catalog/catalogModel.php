f553e02097bf93af036fedc56c9b82923e2781b3
Alexander Yshakov <Ushak0vAlexander@yandex.ru>
2020-05-30 20:27:37 +0400

    fixed product and catalog pages

--- models/catalog/catalogModel.php
+++ models/catalog/catalogModel.php
@@ -6,9 +6,16 @@
         private const DEFAULT_QUANTITY = 6; # Изначально продукты отображаются по 6 на странице
         public static function getProducts($page) 
         {
-            $query = "SELECT `products`.`id_product`, `products`.`title`, `products`.`price`, `products`.`picture`, `products`.`status`, `type`.`typeName` FROM `products`
-            INNER JOIN `type` ON `type`.`id_type` = `products`.`id_type`
-            WHERE `products`.`quantity` <> 0  ORDER BY  `products`.`id_product` LIMIT ".self::DEFAULT_QUANTITY." OFFSET ".$page."; ";
+            // , `products`.`picture`
+            $offset = ($page * self::DEFAULT_QUANTITY - self::DEFAULT_QUANTITY);
+            $query = "SELECT `products`.`id_product`, `products`.`title`, `products`.`price`, `products`.`status`, `type`.`typeName` 
+            FROM `products` 
+            INNER JOIN `type` 
+            ON `type`.`id_type` = `products`.`id_type` 
+            WHERE `products`.`quantity` <> 0 
+            ORDER BY  `products`.`id_product` 
+            LIMIT " . self::DEFAULT_QUANTITY . " OFFSET " . $offset;
+
             $result = self::query($query);
             return $result;
         }
