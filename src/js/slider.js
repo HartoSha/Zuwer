7ff4bab5b10ecbd613341a50c5532530959b0ddd
Alexander Yshakov <Ushak0vAlexander@yandex.ru>
2020-05-31 14:45:45 +0400

    add auto-centering in slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,12 +4,16 @@ const START_INDEX = 1;                                          //Стартов
 const TRANSITION_TIME = 0.8;                                    //Время смещения в секундах
 
                                              
-let slider      = document.getElementById("slider");    
-let leftButton  = slider.querySelector(".slider__leftButton"); 
-let contentRoot = slider.querySelector(".slider__content");     //Находим корневой элемент для контента (тут хранятся сами новости) 
-let rightButton = slider.querySelector(".slider__rightButton");   
-let items       = contentRoot.querySelectorAll(".slider__item"); 
+const slider      = document.getElementById("slider");    
+const leftButton  = slider.querySelector(".slider__leftButton"); 
+const contentRoot = slider.querySelector(".slider__content");     //Находим корневой элемент для контента (тут хранятся сами новости) 
+const rightButton = slider.querySelector(".slider__rightButton");   
+const items       = contentRoot.querySelectorAll(".slider__item"); 
 let currentOffset = START_INDEX;
+
+
+let centering_var = calcCenteringVar();                                     // переменная центровки - дополнительное смещение в %. необходима для выравнивания элементов.
+console.log(`отступ слева для центровки: ${centering_var} %`);
 
 initMove();                                                     // Сдвигаем элементы на стартовую позицию
 
@@ -23,10 +27,11 @@ leftButton.addEventListener("click", function(){
     moveItems();
 });
 
+
 function moveItems(){
     checkOffset();                     
     items.forEach(item => {
-        item.style.transform = `translateX(${-(100 * currentOffset)}%)`;   // Двигаем элементы на 100% их ширины * currentOffset (отрицательное значение, тк.к currentOffset положительный)
+        item.style.transform = `translateX(${-(100 * currentOffset - centering_var)}%)`;   // Двигаем элементы на 100% их ширины * currentOffset (отрицательное значение, тк.к currentOffset положительный)
         console.log("Все элементы смещены на = " + item.style.transform);
     });   
     
@@ -34,7 +39,7 @@ function moveItems(){
 
 function initMove(){ // Если это первое смещение, то смещаем без анимации
     items.forEach(item => {
-        item.style.transform = `translateX(${-(100 * currentOffset)}%)`;    // Двигаем элементы на 100% их ширины * currentOffset
+        item.style.transform = `translateX(${-(100 * currentOffset - centering_var)}%)`;    // Двигаем элементы на 100% их ширины * currentOffset
         console.log("Стартовое смещение = " + item.style.transform);
         setTimeout(function(){ // Ставим стиль с transition'ом для элементов через определенное время, чтобы при загрузке стр. они не двигались 
             item.style.transition = `transform ${TRANSITION_TIME}s`; // transform, чтобы заданное время влияло только на смену слайдов, а не на всё (не на наведение на элементы)
@@ -45,7 +50,7 @@ function initMove(){ // Если это первое смещение, то см
 function checkOffset(){
     console.log("Проверяем offset: " + currentOffset);
 
-    let itemsInRow = Math.round(contentRoot.offsetWidth / items[0].offsetWidth); // Находим кол-во item'ов в строке делая ширину контейнера на ширину 1 item'a и округляя до целого
+    let itemsInRow = Math.round(contentRoot.offsetWidth / items[0].offsetWidth); // Находим кол-во видимых item'ов в строке делая ширину контейнера на ширину 1 item'a и округляя до целого
     if (currentOffset < 0) { // Если смещение меньше 0, то ставим смещение на конец сладера (на последние n видимых элиментов)
         currentOffset = items.length - itemsInRow;
     }
@@ -53,4 +58,17 @@ function checkOffset(){
         currentOffset = 0;
     }
     console.log("itemsInRow = " + itemsInRow);
+}
+
+function calcCenteringVar() { // заметки расчетов https://i.imgur.com/1MoBiKB.png
+    const diffPx = contentRoot.offsetWidth - items[0].offsetWidth; // Разница в px между контейнером и 1 элементом
+    console.log(diffPx); 
+    const offsetToCenter = ((diffPx / 2) / items[0].offsetWidth) * 100; // для центровки делим разницу на 2 и находим ее "количество" в элементе. Умножаем на 100 для получения процентов
+    return offsetToCenter;
+}
+
+window.onresize = () => {
+    centering_var = calcCenteringVar();
+    initMove();
+    console.log("resize");
 }
