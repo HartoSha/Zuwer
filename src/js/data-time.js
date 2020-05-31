663793c07bc6bfc8fa3a30d6114c49659961f9af
Dmitriy Fedorov <dima.fedorov.1406@gmail.com>
2020-05-31 22:57:10 +0400

    footer without images

---
+++ src/js/data-time.js
@@ -0,0 +1,79 @@
+const year = document.querySelector("#clock .year");
+const month = document.querySelector("#clock .month");
+const day = document.querySelector("#clock .day");
+const weekday = document.querySelector("#clock .weekday");
+const hour = document.querySelector("#clock .hour");
+const minutes = document.querySelector("#clock .min");
+
+let date = new Date();
+let dateTotal
+
+function currentDate() {
+  dateTotal = date.getFullYear();
+  year.innerHTML = `${dateTotal}-`;
+
+  dateTotal = date.getMonth();
+  if (dateTotal < 10) {
+    month.innerHTML = `0${dateTotal}-`;
+  } else month.innerHTML = `0${dateTotal}-`;
+
+  dateTotal = date.getDate();
+  if (dateTotal < 10) {
+    day.innerHTML = `0${dateTotal}`;
+  } else day.innerHTML = dateTotal;
+
+  dateTotal = getWeekDay(date);
+  weekday.innerHTML = dateTotal;
+}
+
+function getWeekDay(date) {
+  let days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+
+  return days[date.getDay()];
+}
+currentDate();
+
+function currentTime() {
+  let time = new Date();
+  dateTotal = time.getHours();
+
+  if(dateTotal < 10){
+    hour.innerHTML = `0${dateTotal}:`;
+  }
+  else hour.innerHTML = `${dateTotal}:`;
+
+  dateTotal = time.getMinutes();
+
+  if (dateTotal < 10) {
+    minutes.innerHTML = `0${dateTotal}`;
+  } else minutes.innerHTML = `${dateTotal}`;
+}
+let timerId;
+
+function clockStart() {
+  timerId = setInterval(currentTime, 1000);
+
+  currentTime();
+}
+clockStart();
+
+
+let vid = document.getElementById("tik-tak");
+let swtich = document.getElementById("buttonTikTak");
+
+buttonTikTak.addEventListener("click", TurnOffOnVolume);
+vid.volume = 0;
+function TurnOffOnVolume() {
+  if (swtich.classList.contains("muted")) {
+    vid.volume = 1;
+  } else vid.volume = 0;
+  swtich.classList.toggle("muted");
+}
