ac6a48b1af1ff2f14ae02f3124d9f72eead8a2c7
Alexander Yshakov <Ushak0vAlexander@yandex.ru>
2020-06-03 22:29:22 +0400

    fix telephone input appearence on smartphone

--- views/shared/modalLogRegView.php
+++ views/shared/modalLogRegView.php
@@ -37,17 +37,13 @@ require_once(VIEWS . "shared" . DIRECTORY_SEPARATOR . "headerView.php");
           --><button class="registration-btn"> <span class="btn-text">Регистрация</span> </button>
           </div>
           <form action="/user/register" method="POST" class="modal-reg">
-
-            <input type="text" placeholder="Имя" class="name registration-field" name="reg-name" />
-            <input type="text" placeholder="Фамилия" class="surname registration-field" name="reg-surname" />
-            <input type="text" placeholder="Отчество" class="Middlename registration-field" name="reg-middlename" />
-            <input type="text" placeholder="Логин" class="account-name registration-field" name="reg-account-name" />
-
-            <input type="password" placeholder="Пароль" class="password registration-field" name="reg-pass" />
-            <input type="password" placeholder="Пароль повторно" class="password password-again registration-field" name="reg-pass-again" />
-
-            <input type="text" placeholder="Телефон" class="telephone registration-field" name="reg-telephone" />
-
+            <input type="text" placeholder="Имя" class="registration-field" name="reg-name" />
+            <input type="text" placeholder="Фамилия" class="registration-field" name="reg-surname" />
+            <input type="text" placeholder="Отчество" class="registration-field" name="reg-middlename" />
+            <input type="text" placeholder="Логин" class="registration-field" name="reg-account-name" />
+            <input type="password" placeholder="Пароль" class="registration-field" name="reg-pass" />
+            <input type="password" placeholder="Пароль повторно" class="registration-field" name="reg-pass-again" />
+            <input type="text" placeholder="Телефон" class="registration-field" name="reg-telephone" />
             <button class="registration">
               <span class="btn-text">Зарегистрироваться</span>
             </button>
